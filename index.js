68e7e1c64bec6ef279872c865f8696e56d793411
Hossameldeen <hossamabdalh@gmail.com>
2018-11-22 16:02:18 +0200

    try new output

--- index.js
+++ index.js
@@ -20,9 +20,37 @@ restService.post("/echo", function(req, res) {
   console.log(req.body.queryResult.queryText)
 
   return res.json({
-    speech: "working",
-    displayText: "working",
-    source: "webhook-echo-sample"
+    fulfillmentMessages: [
+      {
+        text: {
+          text: [
+            "This message is from Dialogflow's Cloud Functions for Firebase editor!"
+          ]
+        }
+      },
+      {
+        card: {
+          title: "Title: this is a card title",
+          subtitle: "This is the body text of a card.  You can even use line\n  breaks and emoji! 💁",
+          imageUri: "https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png",
+          buttons: [
+            {
+              text: "This is a button",
+              postback: "https://assistant.google.com/"
+            }
+          ]
+        }
+      },
+      {
+        quickReplies: {
+          quickReplies: [
+            "Quick Reply",
+            "test"
+          ]
+        }
+      }
+    ],
+    outputContexts: []
   });
 });
 
