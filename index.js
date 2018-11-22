2401130b84978b0c1f4b6e33e67da1e1d8782d1c
Hossameldeen <hossamabdalh@gmail.com>
2018-11-22 17:24:32 +0200

    check adding return to callback

--- index.js
+++ index.js
@@ -26,8 +26,7 @@ restService.post("/echo", function(req, res) {
     query: 'nodejs',
     limit: 10
   };
-  
-  
+
   var counter = 0
   scraper.search(options, function(err, url, meta) {
     // This is called for each result
@@ -37,43 +36,71 @@ restService.post("/echo", function(req, res) {
     // console.log(meta.title);
     // console.log(meta.meta);
     // console.log(meta.desc)
+    if(counter == 10){
+      return res.json({
+        fulfillmentMessages: [
+          {
+            text: {
+              text: [
+                "This message is from Dialogflow's Cloud Functions for Firebase editor!"
+              ]
+            }
+          },
+          {
+            card: {
+              title: "Title: this is teeeteteeet card title",
+              subtitle: "This is the body text of a card.  You can even use line\n  breaks and emoji! 💁",
+              imageUri: "https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png",
+              buttons: [
+                {
+                  text: "This is a button",
+                  postback: "https://assistant.google.com/"
+                }
+              ]
+            }
+          }
+        ],
+        outputContexts: []
+      });
+    }
+    
   });
 
-  console.log("callback not just finished")
-  console.log("counter = "+counter)
-  return res.json({
-    fulfillmentMessages: [
-      {
-        text: {
-          text: [
-            "This message is from Dialogflow's Cloud Functions for Firebase editor!"
-          ]
-        }
-      },
-      {
-        card: {
-          title: "Title: this is a card title",
-          subtitle: "This is the body text of a card.  You can even use line\n  breaks and emoji! 💁",
-          imageUri: "https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png",
-          buttons: [
-            {
-              text: "This is a button",
-              postback: "https://assistant.google.com/"
-            }
-          ]
-        }
-      },
-      {
-        quickReplies: {
-          quickReplies: [
-            "Quick Reply",
-            "test"
-          ]
-        }
-      }
-    ],
-    outputContexts: []
-  });
+  // console.log("callback not just finished")
+  // console.log("counter = "+counter)
+  // return res.json({
+  //   fulfillmentMessages: [
+  //     {
+  //       text: {
+  //         text: [
+  //           "This message is from Dialogflow's Cloud Functions for Firebase editor!"
+  //         ]
+  //       }
+  //     },
+  //     {
+  //       card: {
+  //         title: "Title: this is a card title",
+  //         subtitle: "This is the body text of a card.  You can even use line\n  breaks and emoji! 💁",
+  //         imageUri: "https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png",
+  //         buttons: [
+  //           {
+  //             text: "This is a button",
+  //             postback: "https://assistant.google.com/"
+  //           }
+  //         ]
+  //       }
+  //     },
+  //     {
+  //       quickReplies: {
+  //         quickReplies: [
+  //           "Quick Reply",
+  //           "test"
+  //         ]
+  //       }
+  //     }
+  //   ],
+  //   outputContexts: []
+  // });
 });
 
 restService.get("/test", function(req, res){
