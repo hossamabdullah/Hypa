b420aea64c3a01efca0c6e1d8123de867a3b57ba
Hossameldeen <hossamabdalh@gmail.com>
2018-11-22 15:55:45 +0200

    fix missing property of req.body

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ restService.use(bodyParser.json());
 restService.post("/echo", function(req, res) {
   console.log("body")
   console.log(req.body)
-  console.log(req.queryResult.intent.displayName)
-  console.log(req.queryResult.queryText)
+  console.log(req.body.queryResult.intent.displayName)
+  console.log(req.body.queryResult.queryText)
 
   return res.json({
     speech: "working",
