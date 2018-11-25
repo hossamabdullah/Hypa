97b7e1106fee4300c72cf9a0bde3e154ab505852
Hossameldeen <hossamabdalh@gmail.com>
2018-11-25 11:56:05 +0200

    log parameters

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ restService.post("/echo", function(req, res) {
   console.log(req.body.queryResult.intent.displayName)
   console.log(req.body.queryResult.queryText)
   if(req.body.queryResult.intent.displayName == 'GetMe10kDocumentsForCompany'){
-    console.log(req.body)
+    console.log(req.body.queryResult.parameters)
 
   }
 
